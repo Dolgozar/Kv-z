a3dc362761ecdc60b30f4dff1cce5a9cc0829e9f
Dávid Szabó <szabo.david@petrik.hu>
2024-04-10 18:42:28 +0200

    Phase 1

--- Dolgozat/Dolgozat/MainWindow.xaml.cs
+++ Dolgozat/Dolgozat/MainWindow.xaml.cs
@@ -18,11 +18,46 @@ namespace Dolgozat
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
+	/// 
+
 	public partial class MainWindow : Window
 	{
+		ImageBrush myBrush = new ImageBrush();
+
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			//myBrush.ImageSource = new BitmapImage(new Uri("", UriKind.Relative)); //majd valami háttér
+			//this.Background = myBrush;
+			nehez.Visibility = Visibility.Hidden;
+			kozepes.Visibility = Visibility.Hidden;
+			konnyu.Visibility = Visibility.Hidden;
+
 		}
-	}
+
+
+
+
+		private void Start_Click(object sender, RoutedEventArgs e)
+		{ 
+			title.Content = "Válassz egy nehézséget:";
+			next_button.Visibility = Visibility.Hidden;
+            nehez.Visibility = Visibility.Visible;
+            kozepes.Visibility = Visibility.Visible;
+            konnyu.Visibility = Visibility.Visible;
+
+
+        }
+
+        private void nehez_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void konnyu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
 }
